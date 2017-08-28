5bbe97b4f6f976a570fff4990d64b37a4f7c5de9
cavidan666666 <cavidan666666@gmail.com>
2017-08-29 02:19:27 +0400

    final commit

---
+++ WindowsFormsApp6/Form2.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace WindowsFormsApp6
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(151, 327);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Choose Table";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(759, 310);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(122, 40);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Apply";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(335, 327);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(339, 23);
+            this.label1.TabIndex = 2;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(335, 366);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(339, 23);
+            this.label2.TabIndex = 3;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(335, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(339, 23);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Please Choose One of Three Films";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(332, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(339, 23);
+            this.label4.TabIndex = 5;
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1010, 412);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ WindowsFormsApp6/Form2.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form2 : Form
+    {
+        //yaradacaqim duymeleri yerkesdrmek ucun bir buton listi yaradiram
+        public List<Button> butonlar = new List<Button>();
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        //1 - ci butonlari el ile yaradacaqiimz ucun top left deyisenleri teyin edirik
+        int left = 150;
+        int top = 100;
+        //burada countu 1 e beraber edirik ki duymelerde 0 - ci index gorunmesin
+        int count = 1;
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+            //24 eded duyme yaradacaqimiz ucun for  - dan istifade edirik
+            for (int i = 0; i < 24; i++)
+            {
+                //duymeler alt alta sira ile duzulmesi ucun 24 duymeni 4 yere bolub 6 , 6  yazmaq olar 
+                //bunuu etmek ucunde sert qoyuruq
+                if (i == 6)
+                {
+                    left = 150;
+                    top = 150;
+                }
+                else if (i == 12)
+                {
+                    left = 150;
+                    top = 200;
+                }
+                else if (i == 18)
+                {
+                    left = 150;
+                    top = 250;
+                }
+                else if (i == 24)
+                {
+                    left = 150;
+                    top = 300;
+                }
+
+                //Daha Sonra ise Duymemizi yaradmaqa baslayiriq
+                Button menimDuymem = new Button();
+                menimDuymem.Width = 80;
+                menimDuymem.Height = 30;
+                menimDuymem.BackColor = Color.White;
+                menimDuymem.Font = new Font(menimDuymem.Font.Name, menimDuymem.Font.Size, FontStyle.Italic);
+                menimDuymem.Left = left;
+                menimDuymem.Top = top;
+                menimDuymem.Text = count.ToString();
+                Controls.Add(menimDuymem);
+                left += 130;
+                //countu hemise bir vahid artirirqki duymede bir vahid artsin
+                count++;
+                //klik metodu qurmaq ucun  menimDuymeme klik funksiyasi veririk
+                menimDuymem.Click += new System.EventHandler(this.duymeye_bas);
+
+            }
+
+        }
+        private void duymeye_bas(object sender, EventArgs arguments)
+        {
+            Button menimButonum = sender as Button;
+            menimButonum.BackColor = Color.Yellow;
+            //burada yaratdiqim duymeleri buton listine elave edirem
+            butonlar.Add(menimButonum);
+
+            //Daha sonra foreeach la duymeleri gosterirem
+            foreach (var cavcav in butonlar)
+            {
+                //burada eyni duymeye tekrar basilarsa bas verecek hadiseni gostermek ucun duymeyetekararbas teyini edirem
+                cavcav.Click += new System.EventHandler(this.duymeyeTekrarBasilarsa);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            button1.BackColor = Color.LightBlue;
+            label1.Text = "Your Places Selected";
+
+            foreach (var cavcav2 in butonlar)
+            {
+                cavcav2.BackColor = Color.Orange;
+            }
+        }
+
+        //duymeyetekrarbasilarsa burada metod kimi ise dusur
+       private void duymeyeTekrarBasilarsa(object sender, EventArgs argumentss)
+        {
+            foreach (var Cav in butonlar)
+            {
+                label4.Text = "This Table Already Selected";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+
+        {
+            button2.BackColor = Color.LightBlue;
+            foreach (var CavCav in butonlar)
+            {
+                CavCav.BackColor = Color.OrangeRed;
+            }
+
+            label2.Text = " Your Select be Apply Succesfuly";
+        }
+    }
+}
